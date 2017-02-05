1aee251f519bf874e9019806f674ef40e7dd7fa0
Bilych Olexandra <lyubasovasasha@gmail.com>
2017-02-05 16:01:23 +0200

    Remove comments
    Signed-off-by: Bilych Olexandra <lyubasovasasha@gmail.com>

--- boardobject.cpp
+++ boardobject.cpp
@@ -63,10 +63,5 @@ void BoardObject::updateDirection() {
     emit directionChanged(direction);
 }
 
-void BoardObject::paint(QPainter *painter)
-{
-//    QPen pen(m_color, 2);
-//    painter->setPen(pen);
-//    painter->setRenderHints(QPainter::Antialiasing, true);
-//    painter->drawPie(boundingRect().adjusted(1, 1, -1, -1), 90 * 16, 290 * 16);
+void BoardObject::paint(QPainter *painter) {
 }
